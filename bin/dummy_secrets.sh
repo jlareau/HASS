93866b2b565c71b4134421f74d188ceb8925d415
jlareau <joshlareau@gmail.com>
2017-03-29 01:06:28 +0000

    Added groups.yaml and setup the main UI

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "homeassistant_latitude: 51.5488" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "homeassistant_longitude: -3.5958" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "home_elevation: 87.0" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "http_api_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
